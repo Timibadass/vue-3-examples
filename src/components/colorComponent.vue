071bc02c4b44bef1148042228df18dea3e9c3be0
timibadass <omoyenitimi@gmail.com>
2020-11-07 01:06:05 +0100

    add example for provide/inject

---
+++ src/components/colorComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <p :class="[color.value]">This is an example of deeply nested props!</p>
+</template>
+
+<script>
+  export default {
+    inject: ["color"],
+  };
+</script>
+
+<style>
+  .blue {
+    color: blue;
+  }
+  .red {
+    color: red;
+  }
+  .green {
+    color: green;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Vue 3 is liveeeee!" />
+    <select name="color" id="color" v-model="selectedColor">
+      <option value="" disabled selected> Select a color</option>
+      <option :value="color" v-for="(color, index) in colors" :key="index">{{
+        color
+      }}</option></select
+    >
+  </div>
+</template>
+
+<script>
+  import HelloWorld from "@/components/HelloWorld.vue";
+  import { computed } from "vue";
+
+  export default {
+    name: "Home",
+    components: {
+      HelloWorld,
+    },
+    data() {
+      return {
+        selectedColor: "",
+        todos: ["Feed a cat", "Buy tickets"],
+        colors: ["red", "blue", "green"],
+      };
+    },
+    provide() {
+      return {
+        color: computed(() => this.selectedColor),
+      };
+    },
+  };
+</script>
