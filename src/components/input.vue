3de69726f958a396eb20de03674b7d2bae75efa6
timibadass <omoyenitimi@gmail.com>
2020-11-11 11:55:45 +0100

    add fragment example

---
+++ src/components/input.vue
@@ -0,0 +1,22 @@
+<template>
+  <label :for="label" v-bind="$attrs">{{ label }}</label>
+  <input :type="type" :id="label" :name="label" />
+</template>
+
+<script>
+  export default {
+    name: "inputField",
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,36 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Vue 3 is liveeeee!" />
-    <select name="color" id="color" v-model="selectedColor">
+    <!-- <HelloWorld msg="Vue 3 is liveeeee!" /> -->
+    <!-- <select name="color" id="color" v-model="selectedColor">
       <option value="" disabled selected> Select a color</option>
       <option :value="color" v-for="(color, index) in colors" :key="index"
         >{{ color }}
       </option>
     </select>
-    <video-player></video-player>
+    <video-player></video-player> -->
+    <div>
+      <input-component
+        class="awesome__class"
+        label="name"
+        type="text"
+      ></input-component>
+    </div>
   </div>
 </template>
 
 <script>
-  import HelloWorld from "@/components/HelloWorld.vue";
-  import videoPlayer from "@/components/videoComponent.vue";
+  // import HelloWorld from "@/components/HelloWorld.vue";
+  // import videoPlayer from "@/components/videoComponent.vue";
+  import inputComponent from "../components/input.vue";
   import { computed } from "vue";
 
   export default {
     name: "Home",
     components: {
-      HelloWorld,
-      videoPlayer,
+      // HelloWorld,
+      // videoPlayer,
+      inputComponent,
     },
     data() {
       return {
@@ -37,4 +46,8 @@
     },
   };
 </script>
-<style></style>
+<style>
+  .awesome__class {
+    border: 1px solid red;
+  }
+</style>
